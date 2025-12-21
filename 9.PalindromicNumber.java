baf0f750ec5cc2c5ef64a98081166e98624c5efa
Yashraj741 <yashrajkuawat8949@gmail.com>
2025-12-21 11:49:53 +0530

    File uploaded

---
+++ 9.PalindromicNumber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int reversednumber = 0;
+        int originalvariable = x;
+        while(x>0){
+            
+            int lastdigit =x%10;
+            reversednumber = (reversednumber*10)+lastdigit;
+            x=x/10;  
+        }
+        return originalvariable == reversednumber;
+    }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        if(sol.isPalindrome(121)){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
