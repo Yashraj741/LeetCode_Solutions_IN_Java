b800c2fda5f12e78c152aa13f4aa2585a003b443
Yashraj741 <yashrajkumawat8949@gmail.com>
2026-08-01 13:04:31 +0530

    File Added

---
+++ linkedList/243_PalindromeLinkedList.java
@@ -0,0 +1,42 @@
+public class PalindromeLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;  
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null;
+
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+        ListNode first = head; // 1 , 1 2 2 1
+        ListNode second = prev; // 1 , 1 2 2 1
+        while (second != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+}
