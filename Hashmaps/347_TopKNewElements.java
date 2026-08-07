34fde86cc99fddaec2cf2d4de8a4de2823f8dc4d
Yashraj741 <yashrajkumawat8949@gmail.com>
2026-08-07 12:54:36 +0530

    File Added

---
+++ Hashmaps/347_TopKNewElements.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class TopKNewElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        pq.addAll(map.keySet());
+        int[] ans = new int[k];
+        for (int i = 0; i<k; i++) {
+            ans[i] = pq.poll();
+        }
+        return ans;
+    }
+}
